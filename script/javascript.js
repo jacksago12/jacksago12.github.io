3c74790a3cc698f441573dacd99c3f1999c0352b
jacksago12 <jacksonsanchezg@outlook.com>
2017-10-13 16:17:07 -0500

    cambio de carrusel a slaider

--- script/javascript.js
+++ script/javascript.js
@@ -2,5 +2,6 @@
 $(".dropdown-button").dropdown();
 $(".button-collapse").sideNav();
 //carousel//
-$('.carousel.carousel-slider').carousel({fullWidth: true});
+
+$(document).ready(function(){$('.slider').slider();});
      
